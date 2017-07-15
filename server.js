2fd05242628b99fc618887e52c6160531cdb64cd
SubhamSengupta <sengupta335@gmail.com>
2017-07-16 01:52:35 +0530

    modified for heroku deployment

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 const path = require('path');
 const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
-app.listen(3000, () =>{
-  console.log('Server deployed on port 3000');
+app.listen(port, () =>{
+  console.log(`Server deployed on port ${port}``);
 })
