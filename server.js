86b76e55a4a3a4ff90f8af0315fd4f0923b73a9a
SubhamSengupta <sengupta335@gmail.com>
2017-07-16 01:57:02 +0530

    modified

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const path = require('path');
 const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
 app.listen(port, () =>{
-  console.log(`Server deployed on port ${port}``);
+  console.log(`Server deployed on port ${port}`);
 })
