5dc66ad922855a9e4871d4d9b3a50c3b812f44b0
SubhamSengupta <sengupta335@gmail.com>
2017-07-16 22:15:13 +0530

    adding changeStep more pages submit handler

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,26 @@
-
+$('#progress-bar').load('./pages/progress-bar.html');
+$('#main-tab').load('./pages/0.html');
+$('.steps-element:first ').addClass('active-element');
+var currentPage = 0;
 $(document).ready(function () {
-  $('#progress-bar').load('./pages/progress-bar.html');
-  $('#main-tab').load('./pages/basic-info.html');
-  $('.steps-element:first ').addClass('active-element');
-
-  $('#main-tab').ready(function () {
-    $('#next-button').click(function (e) {
-      e.preventDefault();
-      $('.point:first').css('background-color', 'cornflowerblue');
-      $('.line:first').css('background-color', 'cornflowerblue');
-    });
+  $('.forms').submit(function (e){
+    e.preventDefault();
+    //saveData();
+    changeStep();
+    updateProgressBar();
+    loadNextPage();
   });
-  
 });
+function saveData() {
+}
+function changeStep() {
+  $(`#steps .steps-element:eq(${currentPage})`).removeClass('active-element');
+  $(`#steps .steps-element:eq(${currentPage + 1})`).addClass('active-element');
+}
+function updateProgressBar() {
+  $(`#progress-bar-div .line:eq(${currentPage})`).css('background-color', 'cornflowerblue');
+  $(`#progress-bar-div .point:eq(${currentPage + 1})`).css('background-color', 'cornflowerblue');
+}
+function loadNextPage() {
+  return $('#main-tab').load(`./pages/${++currentPage}.html`);
+}
