e48ee75be75ea86f041891658b32add5f3d451e0
Souvik Banerjee <banerjeesouvik@users.noreply.github.com>
2017-07-17 00:02:25 +0530

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+  $('#progress-bar').load('./pages/progress-bar.html');
   $('#main-tab').load('./pages/basic-info.html');
   $('.steps-element:first ').addClass('active-element');
+
+  $('#main-tab').ready(function () {
+    $('#next-button').click(function (e) {
+      e.preventDefault();
+      $('.point:first').css('background-color', 'cornflowerblue');
+      $('.line:first').css('background-color', 'cornflowerblue');
+    });
+  });
+  
 });
