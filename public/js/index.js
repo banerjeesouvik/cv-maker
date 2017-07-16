986a28d2c6071372cc20456f86b5c665283695f6
SubhamSengupta <sengupta335@gmail.com>
2017-07-16 17:54:27 +0530

    added progress bar

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
+
 $(document).ready(function () {
+  $('#progress-bar').load('./pages/progress-bar.html');
   $('#main-tab').load('./pages/basic-info.html');
   $('.steps-element:first ').addClass('active-element');
+  $('#main-tab').ready(function (){
+    $('#next-button').click(function (e) {
+      e.preventDefault();
+      saveData();
+      loadNextPage();
+      $('.point:first').css('background-color', 'cornflowerblue');
+      $('.line:first').css('background-color', 'cornflowerblue');
+    });
+  });
+
 });
+
+function saveData() {
+}
+function loadNextPage() {
+  $('#main-tab').html("<h2>Next page should appear</h2>");
+}
