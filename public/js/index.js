a300d7b5fbd0a6fa20e44eb9caf87cf0c9a5e3ca
SubhamSengupta <sengupta335@gmail.com>
2017-07-16 18:40:06 +0530

    modified js page

--- public/js/index.js
+++ public/js/index.js
@@ -3,20 +3,15 @@ $(document).ready(function () {
   $('#progress-bar').load('./pages/progress-bar.html');
   $('#main-tab').load('./pages/basic-info.html');
   $('.steps-element:first ').addClass('active-element');
-  $('#main-tab').ready(function (){
-    $('#next-button').click(function (e) {
-      e.preventDefault();
-      saveData();
-      loadNextPage();
-      $('.point:first').css('background-color', 'cornflowerblue');
-      $('.line:first').css('background-color', 'cornflowerblue');
-    });
-  });
 
+  $('#main-tab').ready(function () {
+    $('#next-button').click(progressCompletion);
+  });
 });
 
-function saveData() {
-}
-function loadNextPage() {
-  $('#main-tab').html("<h2>Next page should appear</h2>");
+
+function progressCompletion(e) {
+  e.preventDefault();
+  $('.point:first').css('background-color', 'cornflowerblue');
+  $('.line:first').css('background-color', 'cornflowerblue');
 }
