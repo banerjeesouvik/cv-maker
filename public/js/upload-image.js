2358c809e0f6d58165d8be1e7fda112645f71999
SubhamSengupta <sengupta335@gmail.com>
2017-07-18 01:42:59 +0530

    added image upload functionality

---
+++ public/js/upload-image.js
@@ -0,0 +1,18 @@
+
+
+if(window.File && window.FileReader){
+  $('#picture').change(function (e) {
+    var file = e.target.files[0];
+    console.log(e.target.files);
+    if(file.type.indexOf('image') == 0){
+      var reader = new FileReader();
+      console.log(file.type);
+      reader.onload = function (e) {
+          $('#picture-box').css('background-image', 'url(' + e.target.result + ')');
+      }
+      reader.readAsDataURL(file);
+    }
+  });
+}else{
+  console.log('Cannot upload file');
+}
