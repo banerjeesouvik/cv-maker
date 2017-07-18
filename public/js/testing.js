c5aac07172dc74093a4b13e84eb1ba0574c204c1
SubhamSengupta <sengupta335@gmail.com>
2017-07-18 21:58:08 +0530

    added new page and js

---
+++ public/js/testing.js
@@ -0,0 +1,12 @@
+
+$('#zip').focusout(function () {
+  var zipcode = $('#zip').val().trim();
+  var url = `http://postalpincode.in/api/pincode/{zipcode}`;
+  $.getJSON(url, function (json, status, obj) {
+    if(status != "error"){
+      console.log(json);
+    }else{
+      console.log(obj);
+    }
+  });
+});
