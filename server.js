90ba825840c00fd999daaca1aaa4af423e56ec32
Souvik Banerjee <banerjeesouvik@users.noreply.github.com>
2017-07-19 19:33:10 +0530

    Update server.js
    request, body-parser module added, pincode middleware added

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const request=require('request');
+const path = require('path');
+const bodyParser = require('body-parser');
+
 var app = express();
 var port = process.env.PORT || 3000;
-const path = require('path');
+
 const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
+app.use(bodyParser.json());
+
+app.post('/pincode', function (req, res) {
+  var pin=req.body.pincode;
+  request(`http://postalpincode.in/api/pincode/${pin}`,function(error,response,body){
+	if(error){
+		res.status(500).send(err.toString());
+	}
+	if(response){
+		res.status(response.statusCode).send(body.toString());
+	}
+  })
+  
+});
+
 app.listen(port, () =>{
   console.log(`Server deployed on port ${port}`);
 })
