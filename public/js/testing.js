61dd5da2ddac9cab9620a3611002b4d3cfea1dc2
SubhamSengupta <sengupta335@gmail.com>
2017-07-19 20:29:54 +0530

    modified for zip fetch

--- public/js/testing.js
+++ public/js/testing.js
@@ -1,12 +1,35 @@
+var pin=document.getElementById('zip');
+var btn=document.getElementById('btn');
+
+function fill_address(data){
+    var city=document.getElementById('city');
+    var state=document.getElementById('state');
+    var country=document.getElementById('country');
+
+    city.value=data.Circle;
+    state.value=data.State;
+    country.value=data.Country;
+}
 
 $('#zip').focusout(function () {
-  var zipcode = $('#zip').val().trim();
-  var url = `https://postalpincode.in/api/pincode/${zipcode}`;
-  $.getJSON(url, function (json, status, obj) {
-    if(status != "error"){
-      console.log(json);
-    }else{
-      console.log(obj);
+  var xhr=new XMLHttpRequest();
+  xhr.onreadystatechange= function(){
+    if(xhr.readyState===XMLHttpRequest.DONE){
+      if(xhr.status===200){
+        var responseText=JSON.parse(xhr.responseText);
+        if(responseText.Status=='Success'){
+          //alert('Data Fetched Succesfully');
+          fill_address(responseText.PostOffice[0]);
+        }
+        else
+          alert('Invalid Pincode');
+      }
+      else
+        alert('Some error occurred during data fetch');
     }
-  });
+  }
+  xhr.open('POST','/pincode',true);
+  xhr.setRequestHeader('Content-Type','application/json');
+  xhr.send(JSON.stringify({pincode: pin.value}));
+
 });
---
+++ public/js/testing1.js
@@ -0,0 +1,13 @@
+
+$('#zip').focusout(function () {
+  var zipcode = $('#zip').val().trim();
+  var url = `https://postalpincode.in/api/pincode/${zipcode}`;
+  var url = `http://postalpincode.in/api/pincode/${zipcode}`;
+  $.getJSON(url, function (json, status, obj) {
+    if(status != "error"){
+      console.log(json);
+    }else{
+      console.log(obj);
+    }
+  });
+});
