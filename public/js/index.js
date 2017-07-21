96dd15cc0e8e01fcf6cb17860457ddb6ec4ec659
SubhamSengupta <sengupta335@gmail.com>
2017-07-21 23:26:10 +0530

    added on load method for better page loading

--- public/js/index.js
+++ public/js/index.js
@@ -9,13 +9,17 @@ function changeStep() {
   $('.completed-step:last').fadeIn(2000);
 }
 function updateProgressBar() {
-  var index = currentPage;
+  var index = currentPage++;
   $(`.line .progressline:eq(${index})`).animate({width: '100%'},function () {
     $(`.point .progresspoint:eq(${index})`).animate({width: '100%'});
   });
 }
 function loadNextPage() {
-  $('#main-tab').load(`./pages/${++currentPage}.html`);
+  $('#main-tab').load(`./pages/${currentPage + 1}.html`, function() {
+    $('#page-loading').hide();
+    changeStep();
+    updateProgressBar();
+  });
 }
 $(document).ready(function () {
   $('#progress-bar').load('./pages/progress-bar.html');
