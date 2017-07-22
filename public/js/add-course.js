db1afdee7cb193a4fb96232adff2e8560bc98747
SubhamSengupta <sengupta335@gmail.com>
2017-07-23 01:53:31 +0530

    minor fixes

--- public/js/add-course.js
+++ public/js/add-course.js
@@ -1,12 +1,15 @@
 var count = 1;
 $('#add-education').click(function (e) {
   e.preventDefault();
+  $('#loading').css('visibility', 'visible');
   var newCourse = $(`<div class="education" id="new-education-${count++}"></div>`);
   newCourse.load('../pages/new-course.html');
   newCourse.css('visibility', 'hidden');
   var lastCourse = $('.education:last');
   lastCourse.after(newCourse);
   $('#main-tab').ready(function () {
+    $('#loading').css('visibility', 'hidden');
+    $('.education:last').css('visibility', 'visible');
     setTimeout(function () {
       $('span #cgpa:last').addClass('score-active');
     }, 50);
