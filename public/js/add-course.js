015c81de5a41141219fca314eaade37eb7a959cb
SubhamSengupta <sengupta335@gmail.com>
2017-07-23 01:48:19 +0530

    minor bug fixes

--- public/js/add-course.js
+++ public/js/add-course.js
@@ -3,9 +3,12 @@ $('#add-education').click(function (e) {
   e.preventDefault();
   var newCourse = $(`<div class="education" id="new-education-${count++}"></div>`);
   newCourse.load('../pages/new-course.html');
+  newCourse.css('visibility', 'hidden');
   var lastCourse = $('.education:last');
   lastCourse.after(newCourse);
-  setTimeout(function () {
-    $('span #cgpa:last').addClass('score-active');
-  }, 50);
+  $('#main-tab').ready(function () {
+    setTimeout(function () {
+      $('span #cgpa:last').addClass('score-active');
+    }, 50);
+  });
 });
