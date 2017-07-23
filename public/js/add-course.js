b633324d78cf00dc4a95ae54731aa89d062fed13
SubhamSengupta <sengupta335@gmail.com>
2017-07-23 12:39:19 +0530

    modified add course

--- public/js/add-course.js
+++ public/js/add-course.js
@@ -3,14 +3,12 @@ $('#add-education').click(function (e) {
   e.preventDefault();
   $('#page-loading').show();
   var newCourse = $(`<div class="education" id="new-education-${count++}"></div>`);
-  newCourse.load('../pages/new-course.html');
   var lastCourse = $('.education:last');
   lastCourse.after(newCourse);
+  //newCourse.load('../pages/new-course.html');
+  $('.education:last').load('../pages/new-course.html');
+
   $(document.body).ready( function () {
     $('#page-loading').hide();
-    setTimeout( function () {
-      console.log(`#${newCourse.attr('id')}`);
-      $(`#${newCourse.attr('id')} #cgpa`).addClass('score-active');
-    }, 200);
   });
 });
