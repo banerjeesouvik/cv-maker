ffd86a63a5f84b3f90a53bb681ea069b7aa0ce54
banerjeesovik <banerjeesouvik18@hotmail.co.uk>
2017-07-23 23:12:00 +0530

    pin dual request fix

--- public/js/address.js
+++ public/js/address.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     'focusout':function(){
       var value= $(this).val().replace(/[^\d\.]/g, '');
       $(this).val(value);
-      if(value.length == 6){
+      if(value.length == 6 && !pin_valid){
         $('#pin_status').css('background-image', 'url("../images/icons/loading.gif")');
         ajax_call(value);
       }
@@ -39,6 +39,7 @@ function ajax_call(value) {
       if(data_json.status == 'Success'){
         $('#pin_status').css('background-image', 'url("../images/icons/check1.png")');
         fill_address(data_json);
+        pin_valid=true;
         $('input:disabled').prop('disabled', false);
         $('#city').focus();
       }
@@ -53,6 +54,7 @@ function ajax_call(value) {
 
 function disable_field(){
   $('#pin_status').css('background-image', 'url("../images/icons/error.png")');
+  pin_valid=false;
   $('#city').val('');
   $('#dist').val('');
   $('#state').val('');
