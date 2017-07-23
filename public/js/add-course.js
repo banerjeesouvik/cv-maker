afab3daea8a98c49e4aaea729a272b5f0d24e341
SubhamSengupta <sengupta335@gmail.com>
2017-07-23 12:13:15 +0530

    modified education for better loading

--- public/js/add-course.js
+++ public/js/add-course.js
@@ -1,17 +1,16 @@
 var count = 1;
 $('#add-education').click(function (e) {
   e.preventDefault();
-  $('#page-loading').css('visibility', 'visible');
+  $('#page-loading').show();
   var newCourse = $(`<div class="education" id="new-education-${count++}"></div>`);
   newCourse.load('../pages/new-course.html');
-  newCourse.css('visibility', 'hidden');
   var lastCourse = $('.education:last');
   lastCourse.after(newCourse);
-  $('.education:last').ready(function () {
-    $('#page-loading').css('visibility', 'hidden');
-    $('.education:last').css('visibility', 'visible');
-    setTimeout(function () {
-      $('span #cgpa:last').addClass('score-active');
+  $(document.body).ready( function () {
+    $('#page-loading').hide();
+    setTimeout( function () {
+      console.log(`#${newCourse.attr('id')}`);
+      $(`#${newCourse.attr('id')} #cgpa`).addClass('score-active');
     }, 200);
   });
 });
