0c028bfaebe6e629403d9af9eb9b8e8df24a148b
SubhamSengupta <sengupta335@gmail.com>
2017-07-23 23:40:25 +0530

    fix

--- public/js/add-skill.js
+++ public/js/add-skill.js
@@ -1,15 +1,16 @@
 var skillName;
-var skillTab = $('#showskills');
-var newSkill = $('<div class="skillset"></div>');
+
 
 $('#add-skill-form').submit( function (e) {
   e.preventDefault();
   skillName = $('#skill').val();
+  var skillTab = $('#showskills');
+  var newSkill = $('<div class="skillset"></div>');
   $('#skill').val('');
   if( skillName.length != 0){
-    newSkill.load('../pages/skill.html');
     skillTab.append(newSkill);
     $('.skillset:last').fadeIn();
+    $('.skillset:last').load('../pages/skill.html');
   }
 });
 $(document.body).on('click', '#skill-remove', function() {
