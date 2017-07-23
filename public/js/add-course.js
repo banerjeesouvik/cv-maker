18610c268de1f73f394a4880a0d794ad6242f814
SubhamSengupta <sengupta335@gmail.com>
2017-07-23 12:47:37 +0530

    new course minor fixes

--- public/js/add-course.js
+++ public/js/add-course.js
@@ -2,13 +2,8 @@ var count = 1;
 $('#add-education').click(function (e) {
   e.preventDefault();
   $('#page-loading').show();
-  var newCourse = $(`<div class="education" id="new-education-${count++}"></div>`);
+  var newCourse = $(`<div class="education" id="new-education-${count++}" style="display:none"></div>`);
   var lastCourse = $('.education:last');
   lastCourse.after(newCourse);
-  //newCourse.load('../pages/new-course.html');
   $('.education:last').load('../pages/new-course.html');
-
-  $(document.body).ready( function () {
-    $('#page-loading').hide();
-  });
 });
